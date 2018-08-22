4d9d9d03d92be5518b270166d447f96c74c54ef2
tianyi <793187547@qq.com>
2018-08-22 14:27:25 +0800

    facade

---
+++ src/com/ty/patterns/facade/Screen.java
@@ -0,0 +1,11 @@
+package com.ty.patterns.facade;
+
+public class Screen {
+    public void up() {
+        System.out.println("升起银幕....");
+    }
+
+    public void down() {
+        System.out.println("下降银幕....");
+    }
+}
